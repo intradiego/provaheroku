f2c5bb13dac2c15a8bfde37384d833c2136bcedd
Diego Intra <intradiego@gmail.com>
2017-11-05 11:03:22 +0100

    added database access

--- web/index.php
+++ web/index.php
@@ -83,7 +83,7 @@ $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider
 */
 
 // database config
-
+/*
 $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
                array(
                 'pdo.server' => array(
@@ -97,7 +97,7 @@ $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider
                    )
                )
 );
-
+*/
 // query database
 
 $app->get('/db/', function() use($app) {
@@ -116,5 +116,3 @@ $app->get('/db/', function() use($app) {
 });
 
 $app->run();
-
-?>
